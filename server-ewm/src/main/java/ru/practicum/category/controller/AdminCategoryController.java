ba734fa6bdcf0b1c75732689666a0db6e43fa181
Istomin Aleksey <58596713+Kettle-Drummer@users.noreply.github.com>
2024-07-15 01:03:37 +0600

    Update AdminCategoryController.java
    still nothing

--- server-ewm/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
+++ server-ewm/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
@@ -23,22 +23,22 @@ public class AdminCategoryController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CategoryDto createCategory(@Valid @RequestBody NewCategoryDto newCategoryDto) {
+    public CategoryDto add(@Valid @RequestBody NewCategoryDto newCategoryDto) {
         log.info("POST /admin/categories: request create category={}", newCategoryDto);
-        return categoryService.createCategory(newCategoryDto);
+        return categoryService.add(newCategoryDto);
     }
 
     @DeleteMapping("/{catId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCategory(@PathVariable @NotNull @Min(1L) Long catId) {
+    public void delete(@PathVariable @NotNull @Min(1L) Long catId) {
         log.info("DELETE /admin/categories/{catId}: request delete category by id={}", catId);
-        categoryService.deleteCategory(catId);
+        categoryService.delete(catId);
     }
 
     @PatchMapping("/{catId}")
-    public CategoryDto updateCategory(@Valid @RequestBody CategoryDto categoryDto,
+    public CategoryDto update(@Valid @RequestBody CategoryDto categoryDto,
                                       @PathVariable @NotNull @Min(1L) Long catId) {
         log.info("PATCH /admin/categories/{catId}: request update category={} by id={}", categoryDto, catId);
-        return categoryService.updateCategory(categoryDto, catId);
+        return categoryService.update(categoryDto, catId);
     }
 }
