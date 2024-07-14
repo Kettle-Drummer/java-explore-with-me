a065f86941a68280845ff99f15e0a3eb8e11ebc0
Istomin Aleksey <58596713+Kettle-Drummer@users.noreply.github.com>
2024-07-15 01:01:14 +0600

    Update AdminCategoryController.java
    thre's nothing in here

--- server-ewm/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
+++ server-ewm/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
@@ -25,20 +25,20 @@ public class AdminCategoryController {
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto createCategory(@Valid @RequestBody NewCategoryDto newCategoryDto) {
         log.info("POST /admin/categories: request create category={}", newCategoryDto);
-        return categoryService.add(newCategoryDto);
+        return categoryService.createCategory(newCategoryDto);
     }
 
     @DeleteMapping("/{catId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable @NotNull @Min(1L) Long catId) {
         log.info("DELETE /admin/categories/{catId}: request delete category by id={}", catId);
-        categoryService.delete(catId);
+        categoryService.deleteCategory(catId);
     }
 
     @PatchMapping("/{catId}")
     public CategoryDto updateCategory(@Valid @RequestBody CategoryDto categoryDto,
                                       @PathVariable @NotNull @Min(1L) Long catId) {
         log.info("PATCH /admin/categories/{catId}: request update category={} by id={}", categoryDto, catId);
-        return categoryService.update(categoryDto, catId);
+        return categoryService.updateCategory(categoryDto, catId);
     }
 }
